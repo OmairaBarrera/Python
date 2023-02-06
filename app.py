5c1a131b2b66330bfbe363bd8ae7d5614b7a4627
OmairaBarrera <lomaira.barrera@gmail.com>
2023-02-06 13:22:11 -0500

    clase1-python

---
+++ app.py
@@ -0,0 +1,16 @@
+nombre="Omaira Barrera"
+edad=23
+altura=1.58
+datosUnicos=(1098820098, "B+", "08/05/1999")
+pasaTiempo=["Danzar","Ciclismo","Lectura","Tejer"]
+ropa={
+    "chaqueta": True,
+    "Camisa":True,
+    "Gafas":True,
+    "Pantalones":False,
+    "Jeans":True
+}
+
+print(f'Mi nombre es: {nombre}\n Mi edad es: {edad}\n Mi altura es: {altura}' )
+print("DATOS IMPORTANTES")
+print(f'identificación: {datosUnicos[0]}\n Tipo Sanguineo: {datosUnicos[1]}\n Fecha Nacimiento: {datosUnicos[2]}')
---
+++ review1.py
@@ -0,0 +1,21 @@
+
+print("------MENU PRINCIPAL-------")
+print("1. CREAR GRUPO DE ARTEMIS")
+print("1.1. LISTAR CAMPERS DE ARTEMIS")
+print("1.2. AGREGAR CAMPERS A ARTEMIS")
+print("1.3. ELIMINAR CAMPERS DE ARTEMIS")
+print("1.4. ORDENAR ALFABETICAMENTE EN LISTA DE ARTEMIS")
+print("1.5. BUSCAR CAMPERS EN LISTA DE ARTEMIS")
+print("2. CREAR GRUPO DE SPUTNIK")
+print("2.1. LISTAR CAMPERS DE SPUTNIK")
+print("2.2. AGREGAR CAMPERS A SPUTNIK")
+print("2.3. ELIMINAR CAMPERS DE SPUTNIK")
+print("2.4. ORDENAR ALFABETICAMENTE EN LISTA DE SPUTNIK")
+print("2.5. BUSCAR CAMPERS EN LISTA DE SPUTNIK")
+
+seleccion = int(input("Selecciona una opcción: "))
+
+if seleccion == 1:
+    print("seleccionaste 1")
+elif seleccion == 1.1:
+    print("seleccionaste 1.1")
